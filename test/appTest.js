458bcf269e0b9ae19b1d971fe2d4f24fb2dacf91
Jonathan Block <block.jon@gmail.com>
2018-12-20 02:59:07 -0800

    update

--- test/appTest.js
+++ test/appTest.js
@@ -1,7 +1,13 @@
 const assert = require('chai').assert;
 
 describe('App', function() {
-  it('Sanity check', function() {
+  it('Sanity check1', function() {
     assert.equal(1, 1);
+  })
+  it('Sanity check2', function() {
+    assert.equal(2, 2);
+  })
+  it('Sanity check3', function() {
+    assert.equal(3, 3);
   })
 });
